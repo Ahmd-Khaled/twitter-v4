163c25bff839b78d2907f97f68d2823da3b27423
Ahmd-Khaled <ahmd.khaldd@gmail.com>
2023-02-09 00:02:05 +0200

    Create Feed component and its Header

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import { Sidebar } from '@/components'
+import { Feed, Sidebar } from '@/components'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,8 +16,7 @@ export default function Home() {
       </Head>
       <main className='flex min-h-screen max-w-7xl mx-auto'>
         <Sidebar />
-
-        {/* Feed */}
+        <Feed />
 
         {/* Widgets */}
 
